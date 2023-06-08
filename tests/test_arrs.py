2476fb8aeae116a84f14e7cc2192a01ae01a9a28
Snezhana Makhova <makhova-s94@mail.ru>
2023-06-08 23:16:15 +0300

    tests 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,7 @@ def test_get():
     assert arrs.get([], -1, "test") == "test"
     assert arrs.get(-1, -1) == None
 
+
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
